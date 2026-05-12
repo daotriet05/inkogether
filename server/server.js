427c8dec943343566cc0e4ddbe640bb273c9a889
Triet Dao <daohoangminhtriet@gmail.com>
2026-05-12 20:45:37 +0700

    config load balancing with least_conn in nginx

--- server/server.js
+++ server/server.js
@@ -58,54 +58,66 @@ io.on("connection", (socket) => {
   console.log("connected: "+socket.id);
   let socketRoomId = null;
 
+  // listen for creating a new room event
   socket.on("room:create", async (userObject) =>{
     socketRoomId = await handleCreateRoom(io, socket, userObject);
   });
 
+  // listen for joining an existed room event
   socket.on("room:join", (userObject, roomId) => {
     socketRoomId = handleJoinRoom(io, socket, userObject, roomId);
   });
 
+  // listen for changing team event (between team A and B)
   socket.on("room:team_toggle", () => {
     handleTeamToggle(io, socket, socketRoomId);
   });
 
+  // listen for ready/unready event
   socket.on("room:ready_toggle", () => {
     handleReadyToggle(io, socket.id, socketRoomId);
   });
 
+  // listen for new lobby messages
   socket.on("message-lobby:send", (messageObject) => {
     handleMessageLobby(io, socket.id, socketRoomId, messageObject);
   });
 
+  // listen for new team message (broadcast to his team)
   socket.on("message-team:send", (messageObject) => {
     handleMessageTeam(io, socket.id, socketRoomId, messageObject);
   });
 
+  // listen for starting the game event
   socket.on("game:start", async () => {
     await handleGameStart(io, socket.id, socketRoomId);
   });
 
+  // listen for new draws (broadcast to his team)
   socket.on("game:draw:send", (drawObject) => {
     handleDraw(io, socket.id, socketRoomId, drawObject);
   })
 
+  // listen for new guesses (broadcast to his team)
   socket.on("game:guess:send", async (guess) => {
     handleGuess(io, socket.id, socketRoomId, guess)
   })
 
+  // listen for ending the game event
   socket.on("game:end", () => {
     handleGameEnd(io, socket.id, socketRoomId);
   })
 
+  // listen for replaying (new game) event
   socket.on("room:replay", ()=>{
     handleRoomReplay(io, socketRoomId)
   })
 
+  // listen for ending room 
   socket.on("room:end", async () =>{
     await handleEndRoom(io, socketRoomId)
   })
-
+  
   socket.on("disconnect", async () => {
     await handleDisconnect(io, socket.id, socketRoomId)
   });
